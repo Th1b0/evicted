76a279c41331e45289ea416cdec486453a7f7d5d
unknown <btwthibo@gmail.com>
2023-11-20 15:30:56 +0100

    main

--- js/cartPreview.js
+++ js/cartPreview.js
@@ -1,49 +1,43 @@
-const cartNumber = document.querySelector(".number-cart");
-let totalCartitems = localStorage.getItem("totalItems");
+// Select all elements with the class "number-cart"
+const cartNumberElements = document.querySelectorAll(".number-cart");
 
+// Retrieve the total number of items from local storage
+let totalCartItems = localStorage.getItem("totalItems");
+
+// Define your products
 const products = [
-  {
-    id: "1",
-    desc: "Meet the Airpod Pro with ANC – 35dB noise cancellation, seamless iOS integration, and extended battery life.",
-    name: "Airpod Pro",
-    price: 54.99,
-  },
-  {
-    id: "2",
-    desc: "Meet the Airpod Pro 2 with ANC – 35dB noise cancellation, seamless iOS integration, and extended battery life.",
-    name: "Airpod Pro 2",
-    price: 58.99,
-  },
-  {
-    id: "3",
-    desc: "Meet the Airpod 3th Gen, seamless iOS integration.",
-    name: "Airpod 3th Gen",
-    price: 49.99,
-  },
-  {
-    id: "4",
-    desc: "Meet the Airpod 2th Gen, seamless iOS integration.",
-    name: "Airpod 2th Gen",
-    price: 44.99,
-  },
+  // Your product details here...
 ];
 
-if (!totalCartitems) {
-  totalCartitems = 0;
-  cartNumber.textContent = 0;
+// Initialize the cart number for each element
+if (!totalCartItems) {
+  totalCartItems = 0;
+  cartNumberElements.forEach((element) => (element.textContent = 0));
 } else {
-  cartNumber.textContent = totalCartitems;
+  cartNumberElements.forEach(
+    (element) => (element.textContent = totalCartItems)
+  );
 }
 
+// Function to add items to the cart
 function addToCart(id) {
-  const oldNumber = parseInt(cartNumber.textContent);
-  cartNumber.textContent = oldNumber + 1;
+  // Update the cart number for each element
+  cartNumberElements.forEach((element) => {
+    const oldNumber = parseInt(element.textContent);
+    element.textContent = oldNumber + 1;
+  });
 
+  // Find the selected product
   const selectedProduct = products.find((product) => product.id === id);
 
+  // Retrieve and update cart items in local storage
   let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
   cartItems.push(selectedProduct);
-
   localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  localStorage.setItem("totalItems", oldNumber + 1);
+
+  // Update the total number of items in local storage
+  localStorage.setItem("totalItems", parseInt(totalCartItems) + 1);
 }
+
+// Example usage
+// addToCart("1"); // Call this function with the desired product ID
